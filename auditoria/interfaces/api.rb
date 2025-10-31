0c08373f4da21cdb9176dd2846ee328a1ef278ea
Carlos M <carlos.patino@tetraspace.llc>
2025-10-31 10:05:05 -0500

    Fixed docker initializers

--- auditoria/interfaces/api.rb
+++ auditoria/interfaces/api.rb
@@ -1,44 +1,57 @@
-require 'sinatra'
+require 'sinatra/base'
 require 'json'
 require_relative '../infrastructure/http/controllers/auditoria_controller'
 
-controller = Auditoria::Infrastructure::Http::Controllers::AuditoriaController.new
+module Auditoria
+  module Interfaces
+    class API < Sinatra::Base
+      helpers do
+        def controller
+          settings.auditoria_controller
+        end
+      end
 
-set :bind, '0.0.0.0'
-set :port, 5003
+      configure do
+        set :bind, '0.0.0.0'
+        set :port, 5003
+        set :auditoria_controller, Auditoria::Infrastructure::Http::Controllers::AuditoriaController.new
+      end
 
-before do
-  content_type :json
-end
+      before do
+        content_type :json
+      end
 
-get '/health' do
-  JSON.generate(controller.health)
-end
+      get '/health' do
+        JSON.generate(controller.health)
+      end
 
-post '/events' do
-  payload = JSON.parse(request.body.read)
-  resultado = controller.crear(payload)
+      post '/events' do
+        payload = JSON.parse(request.body.read)
+        resultado = controller.crear(payload)
 
-  if resultado.key?(:error)
-    status 422
-    JSON.generate(resultado)
-  else
-    status 201
-    JSON.generate(resultado.transform_keys(&:to_s))
-  end
-end
+        if resultado.key?(:error)
+          status 422
+          JSON.generate(resultado)
+        else
+          status 201
+          JSON.generate(resultado.transform_keys(&:to_s))
+        end
+      end
 
-get '/auditoria/:entidad_id' do
-  entidad_id = params['entidad_id']
-  resultado = controller.listar(entidad_id)
+      get '/auditoria/:entidad_id' do
+        entidad_id = params['entidad_id']
+        resultado = controller.listar(entidad_id)
 
-  if resultado.is_a?(Hash) && resultado.key?(:error)
-    status 422
-    JSON.generate(resultado)
-  else
-    JSON.generate(resultado)
+        if resultado.is_a?(Hash) && resultado.key?(:error)
+          status 422
+          JSON.generate(resultado)
+        else
+          JSON.generate(resultado)
+        end
+      end
+    end
   end
 end
 
 puts 'auditoria ready'
-run! if __FILE__ == $PROGRAM_NAME
+Auditoria::Interfaces::API.run! if __FILE__ == $PROGRAM_NAME
